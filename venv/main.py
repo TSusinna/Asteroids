f08834dafc2dcb4ecad9d9dc8e6e85d43c52e3a0
TSusinna <tumi.pc@hotmail.com>
2025-04-19 20:53:01 +0000

    0.2 Added player

--- venv/main.py
+++ venv/main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 # Se importa la librería de pygame y el contenido de constants.py para utilizarse en este archivo
 
 pygame.init()
@@ -7,18 +8,32 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 # Se inicia pygame y se define el tamaño de la pantalla utilizando las constantes definidas en constants.py
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
-if __name__ == "__main__":
-    main()
-# Se define la función main() que imprime un mensaje de inicio y las dimensiones de la pantalla.
-# Si el script se ejecuta directamente, se llama a la función main()
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    # Se inicia pygame y se define el tamaño de la pantalla utilizando las constantes definidas en constants.py
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            return
-    screen.fill((0, 0, 0))
-    pygame.display.flip()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
+    # Se crea una instancia de la clase Player, que representa al jugador, en el centro de la pantalla
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+        screen.fill((0, 0, 0))
+        player.draw(screen)
+        pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
+        # Se inicia un reloj para controlar la tasa de refresco de la pantalla a 60 FPS
 # Se inicia un bucle infinito que escucha eventos de pygame y cierra la ventana si se recibe un evento de cierre
 # Se llena la pantalla de color negro todo el tiempo
+
+
+
+
+if __name__ == "__main__":
+    main()
+# Si el script se ejecuta directamente, se llama a la función main()
